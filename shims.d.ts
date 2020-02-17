cda6f70b71e9dc4d35e9611d07491f5c2db85ded
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2020-02-17 09:41:41 +0100

    Create shims.d.ts

---
+++ shims.d.ts
@@ -0,0 +1,21 @@
+/**
+     * serial_rb
+     */
+    //% weight=2 color=#002050 icon="\uf287"
+declare namespace serial_rb {
+        /**
+         * Reads multiple characters from the receive buffer and fills a user buffer.
+         * @param length default buffer length, eg: 64
+         */
+        //% blockId=serial_readbuffer block="serial_rb|read buffer %length"
+        //% help=serial/read-buffer weight=5 
+        //%shim=serial_rb::readBuffer
+        export function readBuffer(length: number): Buffer;
+        /**
+         * Sends a buffer through Serial connection
+         */
+        //% blockId=serial_writebuffer block="serial_rb|write buffer %buffer"
+        //% help=serial/write-buffer weight=6 
+        //% shim=serial_rb::writeBuffer
+        export function writeBuffer(buffer: Buffer): void;
+}
