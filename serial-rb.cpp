63cb92b670597015441c0690d1679e0ebec07900
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2020-02-17 09:32:53 +0100

    Create serial-rb.cpp

---
+++ serial-rb.cpp
@@ -0,0 +1,38 @@
+#include "pxt.h"
+
+#define MICROBIT_SERIAL_READ_BUFFER_LENGTH 64
+
+//% weight=2 color=#002050 icon="\uf287"
+namespace serial_rb {
+    /**
+    * Sends a buffer through Serial connection
+    */
+    //% blockId=serial_writebuffer block="serial_rb|write buffer %buffer"
+    //% help=serial/write-buffer weight=6
+    //% shim=serial_rb::writeBuffer
+    void writeBuffer(Buffer buffer) {
+      if (!buffer) return;
+
+      ManagedBuffer buf(buffer);
+      uBit.serial.send(buf.getBytes(), buf.length());
+    }
+    
+    /**
+     * Reads multiple characters from the receive buffer and fills a user buffer.
+     * @param length default buffer length, eg: 64
+     */
+    //% blockId=serial_readbuffer block="serial_rb|read buffer %length"
+    //% help=serial/read-buffer weight=5
+    //% shim=serial_rb::readBuffer
+    Buffer readBuffer(int length) {
+      if (length <= 0)
+        length = MICROBIT_SERIAL_READ_BUFFER_LENGTH;
+        
+      ManagedBuffer buf(length);
+      int read = uBit.serial.read(buf.getBytes(), buf.length());
+      if (read != buf.length())
+        buf = buf.slice(read);
+
+      return buf.leakData();
+    }
+}
